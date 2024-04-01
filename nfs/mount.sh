779c167072af9987a040b9f766165bc34fefaf31
michal pitr <michal.pitr.altis@gmail.com>
2024-04-01 18:41:05 +0200

    create main

--- nfs/mount.sh
+++ nfs/mount.sh
@@ -1,1 +1,7 @@
-sudo mount -v -o vers=4,loud localhost:/ /mnt
+# get ip address of the nfs docker by running docker inspect <container-name>
+# the mount path for nfs4 is relative to the exported root folder. Since we export /data,
+# the path here :/ just means it's inside of /data.
+sudo mount -v -t nfs4 172.17.0.2:/ /mnt
+
+# This might also work
+# sudo mount -v -t nfs4 localhost:/ /mnt
